d021a90fd133824d91ca89981b73a54da16832e2
Jordan Nelson <jnelsontn@gmail.com>
2018-02-19 08:29:08 -0600

    Prevent Feature Reassignment to Projects once initiated.

--- webapp/feature_requests/serializers.py
+++ webapp/feature_requests/serializers.py
@@ -13,6 +13,16 @@ class FeaturesSerializer(serializers.ModelSerializer):
     product_area = serializers.PrimaryKeyRelatedField(queryset=ProductArea.objects.all())
     project = serializers.PrimaryKeyRelatedField(queryset=Projects.objects.all())
 
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.title)
+        instance.priority = validated_data.get('priority', instance.title)
+        instance.target_date = validated_data.get('target_date', instance.target_date)
+        instance.project = instance.project
+        instance.product_area = validated_data.get('product_area', instance.product_area)
+        instance.save()
+        return instance
+
     class Meta:
         model = Features
         fields = ('features_id', 'title', 'description', 'priority',
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -91,7 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
     '127.0.0.1:8080',
